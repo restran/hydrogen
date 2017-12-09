3e0d4dc26a56c56606c6046e5ef5cb576c2e075a
restran <grestran@gmail.com>
2017-12-09 23:10:55 +0800

    remove dependency magnetos

--- hydrogen/utils/__init__.py
+++ hydrogen/utils/__init__.py
@@ -4,6 +4,7 @@ from __future__ import unicode_literals, absolute_import
 
 import logging
 import socket
+import string
 from flask import jsonify
 from importlib import import_module
 
@@ -64,3 +65,28 @@ def import_string(module_path):
         return import_module(module_path)
     except AttributeError:
         return None
+
+
+def get_raw_plain_text(raw_data, decoded_data):
+    """
+    因为密文中可能包含数字，符合等各种非字母的字符，一些解密的算法是不考虑这些
+    在输出明文的时候，要跟这些符合，按要原来的顺序还原回来
+    :param raw_data:
+    :param decoded_data:
+    :return:
+    """
+    index = 0
+    plain = []
+    for i, c in enumerate(raw_data):
+        if c in string.ascii_lowercase:
+            new_c = decoded_data[index].lower()
+            index += 1
+        elif c in string.ascii_uppercase:
+            new_c = decoded_data[index].upper()
+            index += 1
+        else:
+            new_c = c
+
+        plain.append(new_c)
+
+    return ''.join(plain)
